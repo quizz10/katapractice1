3646e00c8801ec9c3fd8b42804aeb0fe0dad2cc7
Johnny Olsen <johnnyolsen90@gmail.com>
2021-12-11 19:49:10 +0100

    Squares finished

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 //        hello(sc.nextLine());
-        System.out.println("Give me a number!");
-        int number = sc.nextInt();
-        even_or_odd(number);
+//        System.out.println("Give me a number!");
+//        int number = sc.nextInt();
+//        even_or_odd(number);
+        System.out.println("How big a square do you want?");
+        square(sc.nextInt());
     }
 
-    private static void even_or_odd(int number) {
-        if (number % 2 == 0) {
-            System.out.println("Even");
-        } else {
-            System.out.println("Odd");
+    private static void square(int squareSize) {
+        for (int i = 0; i < squareSize; i++) {
+            for (int j = 0; j < squareSize; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
         }
+
     }
+
+//    private static void even_or_odd(int number) {
+//        if (number % 2 == 0) {
+//            System.out.println("Even");
+//        } else {
+//            System.out.println("Odd");
+//        }
+//    }
 
 //    public static void hello(String name) {
 //        System.out.println("Hello " + name);
