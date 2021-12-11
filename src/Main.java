8359adb8ed8c3b623d254ad681d760c20e5cc390
Johnny Olsen <johnnyolsen90@gmail.com>
2021-12-11 19:31:28 +0100

    Even or odd finished

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        hello(sc.nextLine());
+//        hello(sc.nextLine());
+        System.out.println("Give me a number!");
+        int number = sc.nextInt();
+        even_or_odd(number);
     }
 
-    public static void hello(String name) {
-        System.out.println("Hello " + name);
+    private static void even_or_odd(int number) {
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
     }
+
+//    public static void hello(String name) {
+//        System.out.println("Hello " + name);
+//    }
 
 }
