7ac2385dbef5691ee49cf65db172cb60149ffb1a
Johnny Olsen <johnnyolsen90@gmail.com>
2021-12-11 20:21:02 +0100

    Longest string finished

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -10,18 +13,33 @@ public class Main {
 //        even_or_odd(number);
 //        System.out.println("How big a square do you want?");
 //        square(sc.nextInt());
-        triangle(sc.nextInt());
+//        triangle(sc.nextInt());
+        findTheLongestString(new String[]{"Bajsmacka", "Kebabapa" ,"Fittkinesåsnehoransgurglande kuvert", "åsnekuk", "nationalencyklopedin"});
     }
 
-    private static void triangle(int triangleSize) {
-        for (int i = 1; i <= triangleSize; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("#");
-
+    private static void findTheLongestString(String[] longestString) {
+        String longest = Arrays.stream(longestString).max(Comparator.comparingInt(String::length)).get();
+        System.out.println(longest);
+        int index = 0;
+        int elementLength = longestString[0].length();
+        for(int i=1; i < longestString.length; i++) {
+            if(longestString[i].length() > elementLength) {
+                index = i;
+                elementLength = longestString[i].length();
             }
-            System.out.println();
         }
+        System.out.println(longestString[index]);
     }
+
+//    private static void triangle(int triangleSize) {
+//        for (int i = 1; i <= triangleSize; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("#");
+//
+//            }
+//            System.out.println();
+//        }
+//    }
 
 //    private static void square(int squareSize) {
 //        for (int i = 0; i < squareSize; i++) {
